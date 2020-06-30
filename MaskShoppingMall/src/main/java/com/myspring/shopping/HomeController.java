c811d42fe44d7d8d5e8476cb7ba795220474841f
taegon <xorhs1510@gmail.com>
2020-06-30 10:59:37 +0900

    마스크 API를 Ajax를 이용한 구현

--- MaskShoppingMall/src/main/java/com/myspring/shopping/HomeController.java
+++ MaskShoppingMall/src/main/java/com/myspring/shopping/HomeController.java
@@ -7,13 +7,20 @@ import java.util.Locale;
 
 import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.myspring.shopping.Bean.BoardBean;
+import com.myspring.shopping.maskAPI.MaskAPI;
+import com.myspring.shopping.service.BoardService;
 import com.myspring.shopping.testdb.TestService;
 import com.myspring.shopping.testdb.TestVO;
 
@@ -24,6 +31,10 @@ import com.myspring.shopping.testdb.TestVO;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final String namespace="com.myspring.shopping.MaskAPIMapper";
+	
+	@Autowired
+	SqlSession sqlSession;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -42,6 +53,19 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/Mask", method = RequestMethod.GET)
+	public String Maks(Locale locale, Model model) {
+	
+		return "Mask";
+	}
+	
+	/*@RequestMapping(value = "/Mask", method = RequestMethod.GET)
+	@ResponseBody
+	public String Mask(Locale locale, Model model) {
+		List<MaskAPI> mask = sqlSession.selectList(namespace + "Select");
+		return new Gson().toJson(mask);
+	}*/
+	
 	@Inject
 	TestService service;
 
@@ -54,5 +78,18 @@ public class HomeController {
 		model.addAttribute("list",list);	
 		
 		return "Test";
+	}
+	
+	@Inject
+	BoardService boardService;
+
+	@RequestMapping(value="/List",method = RequestMethod.GET)
+	public String List(Model model) throws Exception {
+
+		List<BoardBean> list;		
+		list = boardService.list();	
+		model.addAttribute("list",list);	
+		
+		return "/Board/List";
 	}
 }
